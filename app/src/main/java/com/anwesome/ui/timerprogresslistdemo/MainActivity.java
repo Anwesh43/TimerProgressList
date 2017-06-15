d8b72c0da0430dcc044a4d673c35fec1083836f2
Anwesh43 <anweshthecool0@gmail.com>
2017-06-16 02:40:02 +0530

    Using TimerView in app

--- app/src/main/java/com/anwesome/ui/timerprogresslistdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/timerprogresslistdemo/MainActivity.java
@@ -3,11 +3,14 @@ package com.anwesome.ui.timerprogresslistdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.anwesome.ui.timerprogreelist.TimerView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TimerView.create(this,2000,1080);
     }
 }
