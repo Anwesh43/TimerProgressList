f35bdaf598b6c4111f631033c9d793b6e5121e70
Anwesh43 <anweshthecool0@gmail.com>
2017-06-16 14:50:04 +0530

    removing unnecessary packages

--- timerprogreelist/src/main/java/com/anwesome/ui/timerprogreelist/TimerView.java
+++ timerprogreelist/src/main/java/com/anwesome/ui/timerprogreelist/TimerView.java
@@ -9,12 +9,8 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.util.concurrent.ConcurrentLinkedDeque;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * Created by anweshmishra on 16/06/17.
