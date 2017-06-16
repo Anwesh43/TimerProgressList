3fc2c8829f7009f9c0ebe990664460af5acf2b07
Anwesh43 <anweshthecool0@gmail.com>
2017-06-16 14:49:13 +0530

    Using TimerProgressList in app

--- app/src/main/java/com/anwesome/ui/timerprogresslistdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/timerprogresslistdemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.anwesome.ui.timerprogresslistdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.anwesome.ui.timerprogreelist.TimerProgressList;
 import com.anwesome.ui.timerprogreelist.TimerView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TimerView.create(this,2000,1080);
+        TimerProgressList timerProgressList = new TimerProgressList(this);
+        for(int i=0;i<10;i++) {
+            timerProgressList.addTimer(4000);
+        }
+        timerProgressList.show();
     }
 }
